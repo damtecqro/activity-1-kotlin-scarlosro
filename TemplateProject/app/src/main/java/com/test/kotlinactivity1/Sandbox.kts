ce43a1bcac6d9ad67bf1db580e7b524bfa11b28f
Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2020-02-27 11:30:50 -0600

    Subir avance primer actividad

--- TemplateProject/app/src/main/java/com/test/kotlinactivity1/Sandbox.kts
+++ TemplateProject/app/src/main/java/com/test/kotlinactivity1/Sandbox.kts
@@ -1,3 +1,24 @@
 package com.test.kotlinactivity1
 
-println("Hello World")
+val a = readLine();
+
+var index = 1
+var count = 1;
+
+while (index <= (a!!.length-1)){
+
+    if(a[index-1].equals(a[index]) )
+    {
+        count++;
+    }
+    else
+    {
+        println("${a[index-1]} , $count");
+        count=1;
+    }
+
+    index++;
+}
+
+    println("${a!![index-1]} , $count");
+
--- TemplateProject/app/src/test/java/com/test/kotlinactivity1/ExampleUnitTest.kt
+++ TemplateProject/app/src/test/java/com/test/kotlinactivity1/ExampleUnitTest.kt
@@ -9,9 +9,16 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
+fun <T> encode(list: List<T>): List<Pair<Int, T>> =
+    pack(list).map { Pair(it.size, it.first()) }
+
+
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @Test fun `run-length encoding of a list`() {
+        assertThat(encode("aaaabccaadeeee".toList()), equalTo(listOf(
+            Pair(4, 'a'), Pair(1, 'b'), Pair(2, 'c'), Pair(2, 'a'), Pair(1, 'd'), Pair(4, 'e')
+        )))
     }
 }
